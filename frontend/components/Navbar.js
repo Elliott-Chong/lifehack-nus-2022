208b9927246713666a96ad7e3acc4b37328f779d
Elliott Chong <elliottchong16@gmail.com>
2022-07-09 19:00:10 +0800

    die

--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 function Navbar() {
   return (
     <nav className="font-noteworthy flex-col sticky bg-white top-0 font-bold md:flex-row flex px-20 py-2">
-      <h1 className="md:text-3xl text-xl">A Unicorn's Treasure</h1>
+      <h1 className="md:text-3xl md:m-0 mb-2 text-xl">A Unicorn's Treasure</h1>
       <div className="md:ml-auto sm:m-0 md:flex-row flex-col flex gap-2">
         <Link href="/login">
           <a className="px-4 py-2 text-center bg-btn-green rounded-md shadow-md hover:shadow-lg cursor-pointer">
